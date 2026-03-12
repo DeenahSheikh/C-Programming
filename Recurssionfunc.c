26822d49610d3b805f9e9e3a16de62229f0998f3
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-03-12 23:06:19 +0530

    question

---
+++ Recurssionfunc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sumToN(int n){
+    if(n==1){
+        return 1;
+    }
+    else{
+        return n+sumToN(n-1);
+    }
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    printf("%d", sumToN(n));
+    return 0;
+}
