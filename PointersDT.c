dd5bcfef3b6151622f4898f23d7fdf1557d5c6fd
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-03-23 11:07:42 +0530

    Pointers

---
+++ PointersDT.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a=5;
+    float b=6.5f;
+    char c='a';
+ 
+    int *p1=&a;
+    float *p2=&b;
+    char *p3=&c;
+
+    printf("\n %p,[%d], %d", p1,*p1,sizeof(a));
+    printf("\n %p,[%f], %d", p2,*p2,sizeof(b));
+    printf("\n %p,[%c], %d", p3,*p3,sizeof(c));
+    return 0;
+}
