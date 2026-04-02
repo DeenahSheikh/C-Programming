a0d7156d376fbb14493a0bee04c7b8ce0ad80084
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-04-02 22:43:35 +0530

    Pointers

---
+++ Pointer arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int data[5]={12,24,36,48,60};
+    int *ptr=data;
+    for(int i=0; i<5;i++){
+        printf("Value: %d, Address: %p\n", *ptr, (void*)ptr);
+        ptr++;
+    }
+
+    ptr=data;
+    printf("Third element: %d\n", *(ptr +2));
+    printf("Last element: %d\n", *(ptr +4));
+
+    return 0;
+}
