db76e71d18d0410bde21b3822ae7cbae44e8d329
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-04-14 21:13:45 +0530

    Structs

---
+++ intial struct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+struct Employee{
+    int id;
+    char department[15];
+    int yearsOfService;
+    float salary;
+};
+int main() {
+    struct Employee emp1={101,"Engineering", 5, 75000.50};
+    struct Employee emp2={102,"Marketing", 3, 65000.25};
+    struct Employee emp3={103,"Sales", 8, 80000.75};
+
+    printf("Employee 1:\n");
+    printf("ID: %d\n", emp1.id);
+    printf("Department: %s\n", emp1.department);
+    printf("Years of Service: %d\n", emp1.yearsOfService);
+    printf("Salary: %.2f\n",emp1.salary);
+
+    printf("Employee 2:\n");
+    printf("ID: %d\n", emp2.id);
+    printf("Department: %s\n", emp2.department);
+    printf("Years of Service: %d\n", emp2.yearsOfService);
+    printf("Salary: %.2f\n",emp2.salary);
+
+    printf("Employee 3:\n");
+    printf("ID: %d\n", emp3.id);
+    printf("Department: %s\n", emp3.department);
+    printf("Years of Service: %d\n", emp3.yearsOfService);
+    printf("Salary: %.2f\n",emp3.salary);
+    float total= emp1.salary + emp2.salary +emp3.salary;
+    printf("Total Salary: %.2f\n", total);
+
+    float average= (emp1.yearsOfService +emp2.yearsOfService+ emp3.yearsOfService) / 3.0;
+    printf("Average Years of Service: %.2f", average);
+
+    return 0;
+}
