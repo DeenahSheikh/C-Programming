40884c079ca3a6e49af70ed5ee863b70bb731d5d
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-04-29 23:48:58 +0530

    Question

--- height.c
+++ height.c
@@ -9,22 +9,34 @@ void add(int feet1, int inches1,int feet2,int inches2 ,int *tf, int *ti){
         *ti=*ti-12;
         *tf+=1;
     }
-
-
+}
+void sub(int feet1, int inches1,int feet2,int inches2 ,int *rf, int *ri){
+    //1 1 0 2
+    *rf=feet1-feet2;
+    *ri=inches1-inches2;
+    if(*ri<0){
+        *ri=12+*ri;
+        *rf-=1;
+    }
 
 }
+
+
 int main()
 {
     int feet1, inches1, feet2, inches2;
     int afeet,ainches;
+    int sfeet,sinches;
     printf("Enter the first height (feet and inches): ");
     scanf("%d %d", &feet1, &inches1);
     printf("Enter the second height (feet and inches): ");
     scanf("%d %d", &feet2, &inches2);
 
     add(feet1,inches1,feet2,inches2,&afeet,&ainches);
+    sub(feet1,inches1,feet2,inches2,&sfeet,&sinches);
 
-    printf("sum = %d feet %d inches",afeet,ainches);
+    printf("sum = %d feet %d inches\n",afeet,ainches);
+    printf("sub = %d feet %d inches",sfeet,sinches);
 
     return 0;
 }
