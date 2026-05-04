63eb0616dcddfd93534ebed7727c92ce4af0fa08
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-05-04 22:25:24 +0530

    Practice

---
+++ Practice/find 3rd smallest.c
@@ -0,0 +1,19 @@
+//1) wap to find the 3 smallest using array of n numbers
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int findthrird(const void *a, const void *b){
+    return (*(int*) a- *(int*)b);
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    qsort(arr,n,sizeof(int),findthrird);
+    printf("%d",arr[2]);
+}
