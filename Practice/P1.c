b4e6a866c03e9358c1590bcd732da58e6d4e2e44
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-05-29 22:56:36 +0530

    Practice

---
+++ Practice/P1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+
+//calcualte total compound interest formula is sent on whatsapp
+
+// if principal amount is more than 50000 than add 1 to rate;
+// else print normal CI
+double calculateCompoundInterest(double P, double R, int T) {
+    float temp;
+    double CI;
+
+    if(P>50000){
+        temp=(1+(R+1)/100);
+        CI=P*pow(temp,T)-P;
+    }
+    else{
+        temp=(1+ R/100);
+        CI=P*pow(temp,T)-P;
+    }
+}
+
+int main() {
+    double P, R;
+    int T;
+
+    scanf("%lf %lf %d", &P, &R, &T);
+
+    double result = calculateCompoundInterest(P, R, T);
+
+    printf("%.2f\n", result);
+
+    return 0;
+}
